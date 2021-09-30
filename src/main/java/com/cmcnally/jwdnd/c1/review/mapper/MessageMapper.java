4d637df81608ab5b18c8fe2a289d617b374abad5
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-09-30 17:26:03 +0100

    Added User and Message MyBatis mappers

---
+++ src/main/java/com/cmcnally/jwdnd/c1/review/mapper/MessageMapper.java
@@ -0,0 +1,7 @@
+package com.cmcnally.jwdnd.c1.review.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MessageMapper {
+}
---
+++ src/main/java/com/cmcnally/jwdnd/c1/review/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.cmcnally.jwdnd.c1.review.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+}
