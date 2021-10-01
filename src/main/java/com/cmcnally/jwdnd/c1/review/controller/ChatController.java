6f6e3d31ff8a08d43aabeee4db2ceb5273c2b013
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-10-01 18:01:01 +0100

    Removed username input from chat html. Updated ChatController to use authenticated username when adding a chat message

--- src/main/java/com/cmcnally/jwdnd/c1/review/controller/ChatController.java
+++ src/main/java/com/cmcnally/jwdnd/c1/review/controller/ChatController.java
@@ -3,6 +3,8 @@ package com.cmcnally.jwdnd.c1.review.controller;
 import com.cmcnally.jwdnd.c1.review.model.ChatForm;
 import com.cmcnally.jwdnd.c1.review.model.ChatMessage;
 import com.cmcnally.jwdnd.c1.review.service.MessageService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,20 +17,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ChatController {
 
     private MessageService messageService;
+    // Authentication variable used to get information on the authenticated user
+    private Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+    // Constructor
     public ChatController(MessageService messageService) {
         this.messageService = messageService;
     }
 
+    // GET method to get the chat page
     @GetMapping
     public String getChatPage(@ModelAttribute("newChatMessage") ChatForm newChatMessage, Model model){
         model.addAttribute("chatMessages", this.messageService.getChatMessages());
         return "chat";
     }
 
+    // POST method to handle a user posting a chat message
     @PostMapping
     public String postChatMessage(@ModelAttribute("newChatMessage") ChatForm chatForm, Model model){
-        messageService.addMessage(new ChatMessage(chatForm.getUsernameText(), chatForm.getMessageText(), chatForm.getModeSelection()));
+        messageService.addMessage(new ChatMessage(authentication.getName(), chatForm.getMessageText(), chatForm.getModeSelection()));
         model.addAttribute("chatMessages", this.messageService.getChatMessages());
         chatForm.setMessageText("");
         chatForm.setUsernameText("");
